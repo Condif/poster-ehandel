f46efc209aa214fda946879351cc235d200cdfa8
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-05-22 16:25:04 +0200

    userRoutes and controllers

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,1 +1,45 @@
 // Add user controllers here
+
+const User = require('../models/userModel')
+
+// Get all users
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+}
+
+exports.getUserById = async (req, res) => { 
+    try {
+        const user = await User.findOne({ _id: req.params._id });
+        console.log(user);
+        
+        res.json(user);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+}
+
+
+exports.registerNewUser = async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+        deliveryAddress: req.body.deliveryAddress,
+      });
+      try {
+        const newUser = await user.save();
+        res.status(201).json({ newUser });
+      } catch (err) {
+        // res.status.apply(400).json({ message: err.message })
+        res.status(400).json({ message: err.message });
+      }
+}
+    
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -2,38 +2,17 @@ const express = require("express");
 const mongoose = require('mongoose');
 const router = express.Router();
 const User = require("../models/userModel");
+const controller = require('../controllers/userControllers')
 
 // const getUser = require("../Middlewares/getUser.js");
 
 //Getting all
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/", controller.getAllUsers)
 
+//Getting one
+router.get("/:_id", controller.getUserById)
 
 //Creating one
-router.post("/", async (req, res) => {
-    
-    const user = new User({
-      name: req.body.name,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      password: req.body.password,
-      role: req.body.role,
-      deliveryAddress: req.body.deliveryAddress,
-    });
-    try {
-      const newUser = await user.save();
-      res.status(201).json({ newUser });
-    } catch (err) {
-      // res.status.apply(400).json({ message: err.message })
-      res.status(400).json({ message: err.message });
-    }
-  });
+router.post("/register", controller.registerNewUser)
 
-  module.exports = router;
+module.exports = router;
