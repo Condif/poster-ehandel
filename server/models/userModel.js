9278d3fa2f4023da49bfebdbd038e0144b02cfa8
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-05-22 16:44:48 +0200

    bcrypt

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
 const Address = require("../schemas/addressSchema");
 
 const userSchema = new mongoose.Schema({
@@ -32,5 +35,25 @@ const userSchema = new mongoose.Schema({
     deliveryAddress: [Address]
 })
 
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 module.exports = mongoose.model('User', userSchema)
