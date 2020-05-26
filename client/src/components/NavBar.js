15069495b615e786cb1c232e7159fbd1b80ac5e0
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-05-26 18:54:35 +0200

    Knapp för att öppna Cart från navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+// Hämta useContext för att använda funktioner i UserContext.
+import React, {useContext} from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Button,
+    Grid,
+    Typography,
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import lightGreen from '@material-ui/core/colors/lightGreen';
 import teal from '@material-ui/core/colors/teal';
+import {UserContext}  from '../contexts/UserContext';
+
 
 const NavAppBar = withStyles({
     root: {
@@ -27,6 +34,9 @@ const NavButton = withStyles({
 
 
 const NavBar = () => {
+    
+    // Hämta openCart funktionen från UserContext
+    const { openCart } = useContext(UserContext)
     return (
         <NavAppBar position="static">
             <Toolbar>
@@ -37,6 +47,12 @@ const NavBar = () => {
                     <NavButton aria-label="category3">Category3</NavButton>
                     <NavButton aria-label="category4">Category4</NavButton>
                     <NavButton aria-label="category5">Category5</NavButton>
+                    <Button
+                        color="default"
+                        onClick={openCart}
+                    >
+                        <Typography variant="overline">Cart</Typography>
+                    </Button>
                 </Categories>
                 <Grid item>
                     <NavButton aria-label="sign up">Sign up</NavButton>
