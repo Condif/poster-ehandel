c007e958b388dc4bcfd23c5464136348424307ac
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-05-26 18:55:07 +0200

    UserContext setup med cartModal state och funktion

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { createContext, Component } from "react";
+export const UserContext = createContext();
 
-const UserContext = () => {
-    return (
-        <p>UserContext</p>
-    )
+class UserContextProvider extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // Allt här kommer vi åt med consumer
+            isCartOpen: false,
+            openCart: this.openCart
+        }
+    }
+
+    //State för cartModal
+    openCart = () => {   
+        this.setState({ isCartOpen: !this.state.isCartOpen})
+    }
+
+    // Logga in
+    // Logga ut
+    // Se senaste beställning
+
+    render() {
+        return (
+            <UserContext.Provider value={{ ...this.state }}>
+                {this.props.children}
+            </UserContext.Provider>
+        );
+    }
+
 }
 
-// Logga in
-// Logga ut
-// Se senaste beställning
-
-export default UserContext;
+export default UserContextProvider;
