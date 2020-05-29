6d1ad8e97228915933d85d7cad3d1ac672b313d2
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-05-29 10:57:36 +0200

    create syntax to use productcard in 4 places

---
+++ client/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,48 @@
+import React, { useContext} from 'react';
+import { useHistory } from 'react-router-dom';
+import { Grid, Card, CardHeader, CardActions, CardActionArea } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import useStyles from './ProductCardStyles';
+import { UserContext } from '../../contexts/UserContext';
+
+const ProductCard = (props) => {
+    const classes = useStyles();
+    const history = useHistory();
+    const { product, path } = props;
+    const { addToCart } = useContext(UserContext)
+    const handleClick = () => {
+        addToCart(product)
+        return history.push(path)
+    }
+    const handleAddToCart = () => {    
+        addToCart(product)
+    }
+
+    return (
+        <Grid item>
+            <Card>
+                <CardActionArea>
+                    <CardHeader title={product.name} titleTypographyProps={{ variant: 'h6' }} classes={props.case === 'cart' ? {title: classes.titleCart  } : { title: classes.title  }} />
+                    <Typography>
+                        {product.name}
+                    </Typography>
+                    <Typography>{product.description}</Typography>
+                    <Typography>
+                        {product.price}SEK
+                    </Typography>
+                </CardActionArea>
+                <CardActions>
+                    {props.case === 'main' ? 
+                        <Button size="small" onClick={handleClick}>View product</Button> &&
+                        <Button size="small" onClick={handleAddToCart}>Add to cart</Button> : null}
+                    {props.case === 'cart' ? <Typography>test</Typography> : null}
+                </CardActions>
+            </Card>
+        </Grid>
+    )
+}
+
+
+
+export default ProductCard;
---
+++ client/src/components/ProductCard/ProductCardStyles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    title: {
+        margin: "2px"
+    
+    },
+    titleCart: {
+        fontSize: "6px"
+    
+    }
+});
+
+export default useStyles;
