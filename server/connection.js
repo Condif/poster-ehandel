9c354589f7b30879e832b4d82cdb6ce6c601667c
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-06-01 14:35:05 +0200

    Started making changes on Updateinventory

--- server/connection.js
+++ server/connection.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set("useCreateIndex", true)
+mongoose.set("useCreateIndex", true);
 
-mongoose.connect("mongodb+srv://anonymous_chinchilla:anonymous_chinchilla@poster-ehandel-savbu.mongodb.net/poster-ehandel", {
+mongoose.connect(
+  "mongodb+srv://anonymous_chinchilla:anonymous_chinchilla@poster-ehandel-savbu.mongodb.net/poster-ehandel",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-
-db.on('error', (error) => console.log(error))
-db.once('open', () => console.log('Connected to Database'))
-
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database"));
