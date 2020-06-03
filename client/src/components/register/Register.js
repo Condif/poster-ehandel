e02982d5d346701c9dbdde418ea7301563ca58c7
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-06-03 16:38:32 +0200

    Validation on email and zipcode

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
 import { useHistory } from "react-router-dom";
 import useStyles from "./RegisterStyles";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
 const Register = () => {
   const [inputValues, setInputValues] = useState({
@@ -20,6 +21,10 @@ const Register = () => {
   const classes = useStyles();
 
   const history = useHistory();
+
+  function handleSubmit() {
+    createNewUser();
+  }
 
   function redirectToLogin() {
     history.push("/login");
@@ -69,105 +74,110 @@ const Register = () => {
 
   return (
     <Container className={classes.flexedContainer} maxWidth="sm">
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="text"
-        required
-        label="Name"
-        value={inputValues.name}
-        onChange={(event) => handleInputChange(event, "name")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="text"
-        required
-        label="Lastname"
-        value={inputValues.lastname}
-        onChange={(event) => handleInputChange(event, "lastname")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="email"
-        required
-        label="E-mail"
-        value={inputValues.email}
-        onChange={(event) => handleInputChange(event, "email")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="password"
-        required
-        label="Password"
-        value={inputValues.password}
-        onChange={(event) => handleInputChange(event, "password")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="text"
-        required
-        label="Adress"
-        value={inputValues.address}
-        onChange={(event) => handleInputChange(event, "address")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="text"
-        required
-        label="Zipcode"
-        inputProps={{
-          maxLength: 5,
-          // pattern: "[0-9]+",
-        }}
-        value={inputValues.zipcode}
-        onChange={(event) => handleInputChange(event, "zipcode")}
-      ></TextField>
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        size="small"
-        type="text"
-        required
-        label="City"
-        value={inputValues.city}
-        onChange={(event) => handleInputChange(event, "city")}
-      ></TextField>
-      <Button
-        disabled={
-          !inputValues.name ||
-          !inputValues.lastname ||
-          !inputValues.email ||
-          !inputValues.password ||
-          !inputValues.address ||
-          !inputValues.zipcode ||
-          !inputValues.city
-        }
-        className={classes.submitButton}
-        variant="contained"
-        color="primary"
-        fullWidth
-        onClick={() => createNewUser()}
+      <ValidatorForm
+        onSubmit={handleSubmit}
+        onError={(errors) => console.log(errors)}
       >
-        Sign up
-      </Button>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          type="text"
+          label="Name"
+          value={inputValues.name}
+          onChange={(event) => handleInputChange(event, "name")}
+        ></TextField>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          type="text"
+          label="Lastname"
+          value={inputValues.lastname}
+          onChange={(event) => handleInputChange(event, "lastname")}
+        ></TextField>
+        <TextValidator
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          label="Email"
+          onChange={(event) => handleInputChange(event, "email")}
+          name="email"
+          value={inputValues.email}
+          validators={["required", "isEmail"]}
+          errorMessages={["this field is required", "email is not valid"]}
+        />
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          type="password"
+          label="Password"
+          value={inputValues.password}
+          onChange={(event) => handleInputChange(event, "password")}
+        ></TextField>
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          type="text"
+          label="Adress"
+          value={inputValues.address}
+          onChange={(event) => handleInputChange(event, "address")}
+        ></TextField>
+        <TextValidator
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          label="Zipcode"
+          onChange={(event) => handleInputChange(event, "zipcode")}
+          name="zipcode"
+          inputProps={{
+            maxLength: 5,
+          }}
+          value={inputValues.zipcode}
+          validators={[
+            "minNumber:0",
+            "maxNumber:99999",
+            "matchRegexp:^[0-9]+$",
+          ]}
+          errorMessages={["only numbers"]}
+        />
+        <TextField
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          size="small"
+          type="text"
+          label="City"
+          value={inputValues.city}
+          onChange={(event) => handleInputChange(event, "city")}
+        ></TextField>
+        <Button
+          type="submit"
+          disabled={
+            !inputValues.name ||
+            !inputValues.lastname ||
+            !inputValues.email ||
+            !inputValues.password ||
+            !inputValues.address ||
+            !inputValues.zipcode ||
+            !inputValues.city
+          }
+          className={classes.submitButton}
+          variant="contained"
+          color="primary"
+          fullWidth
+        >
+          Submit
+        </Button>
+      </ValidatorForm>
     </Container>
   );
 };
