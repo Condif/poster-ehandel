80009494558f1c03b91909d6685243d2a2fe7d61
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-06-08 14:56:54 +0200

    AllOrders responsive

--- client/src/components/Orders/Orders.js
+++ client/src/components/Orders/Orders.js
@@ -7,75 +7,83 @@ const Orders = () => {
 
   const [orders, setOrders] = useState();
 
-
   const getAllOrders = async () => {
     const allOrders = await fetch("http://localhost:8080/api/orders", {
       method: "GET",
       credentials: "include",
-    }).then((response) => response.json())
+    })
+      .then((response) => response.json())
       .then((data) => {
-        return data
-      })
-    return allOrders
+        return data;
+      });
+    return allOrders;
   };
 
   const setupOrders = async () => {
-    const allOrders = await getAllOrders()
-    setOrders(allOrders)
-  }
+    const allOrders = await getAllOrders();
+    setOrders(allOrders);
+  };
 
   useEffect(() => {
-    setupOrders()
+    setupOrders();
   }, []);
 
   return (
-    <Grid container className={classes.mainDiv}>
-      <Grid item xs={12}>
-        <Typography variant="h3">Orders</Typography>
-      </Grid>
-      {orders != undefined ? (
-        orders.map((order) => (
-          <Grid container key={order._id}className={classes.orderContainer}>
-            <Paper className={classes.paper}style={{ width: " 100%" }}>
-              <Grid item xs={12} className={classes.information}>
-                <Typography variant="h4">Order: {order._id} </Typography>
-              </Grid>
-              <Grid container className={classes.orderItems}>
-                <Grid item xs={4} className={classes.information}>
-                  <Typography variant="h5">Information: </Typography>
-                  <Typography>Namn: {order.name}</Typography>
-                  <Typography>Efternamn: {order.lastname}</Typography>
+    <Grid container>
+      {orders != undefined
+        ? orders.map((order) => (
+            <Grid container key={order._id} className={classes.orderContainer}>
+              <Paper className={classes.paper} style={{ width: " 100%" }}>
+                <Grid item xs={12} className={classes.information}>
+                  <Typography className={classes.heading} variant="h6">
+                    Order: {order._id}{" "}
+                  </Typography>
                 </Grid>
-                <Grid item xs={4} >
-                  <Grid container className={classes.products}>
-                    <Grid item xs={12}>
-                      <Typography variant="h5">Products:</Typography>
-                    </Grid>
-                    {order.products[0].map((product) => (
-                      <Grid item xs={12} className={classes.orderPaper} key={product._id}>
-                        <Typography className={classes.title}>{product.name}</Typography>
+                <Grid container className={classes.orderItems}>
+                  <Grid item xs={12} sm={4} className={classes.information}>
+                    <Typography variant="h6">Information: </Typography>
+                    <Typography>Name: {order.name}</Typography>
+                    <Typography>Lastname: {order.lastname}</Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Grid container className={classes.products}>
+                      <Grid item xs={12}>
+                        <Typography variant="h6">Products:</Typography>
                       </Grid>
-                    ))}
+                      {order.products[0].map((product) => (
+                        <Grid
+                          item
+                          xs={12}
+                          className={classes.orderPaper}
+                          key={product._id}
+                        >
+                          <Typography className={classes.title}>
+                            {product.name}
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12} sm={4} className={classes.delivery}>
+                    <Typography variant="h6"> Delivery:</Typography>
+                    <Typography>
+                      Alternative: {order.shipment.alternative}
+                    </Typography>
+                    <Typography>Cost: {order.shipment.cost} SEK</Typography>
+                    <Typography>
+                      Days to delivery: {order.shipment.deliveryTime}
+                    </Typography>
                   </Grid>
                 </Grid>
-                <Grid item xs={4} className={classes.delivery}>
-                  <Typography variant="h5"> Delivery:</Typography>
-                  <Typography>Alternative: {order.shipment.alternative}</Typography>
-                  <Typography>Cost: {order.shipment.cost}</Typography>
-                  <Typography>Days to delivery: {order.shipment.deliveryTime}</Typography>
+                <Grid container className={classes.total}>
+                  <Typography>Total cost: {order.totalPrice} SEK </Typography>
                 </Grid>
-              </Grid>
-            </Paper>
-            <Grid container className={classes.total} justify="space-between">
-              <Typography>Total:</Typography>
-              <Typography>{order.totalPrice}</Typography>
+              </Paper>
             </Grid>
-          </Grid>
-        ))) : null}
+          ))
+        : null}
     </Grid>
-  )
-}
+  );
+};
 
-
-
-export default Orders
+export default Orders;
--- client/src/components/Orders/OrdersStyles.js
+++ client/src/components/Orders/OrdersStyles.js
@@ -1,30 +1,27 @@
 import { makeStyles } from "@material-ui/core";
+import green from "@material-ui/core/colors/green";
 
 const useStyles = makeStyles((theme) => ({
-    mainDiv: {
-        background: "grey"
-    },
-    orderContainer: {
-        paddingTop: theme.spacing(6)
-        
-    },
-    paper: {
-        
-    },
-    orderItems: {
-        justifyContent: "center",
-        paddingTop: theme.spacing(3)
-    },
-    information: {
-    },
-    products: {
-
-    },
-    delivery: {
-    },
-    total: {
-        background: "white"
-    }
+  heading: {
+    background: `${green[800]}`,
+    color: "white",
+    padding: "1rem",
+  },
+  paper: {},
+  orderItems: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: theme.spacing(2),
+    marginLeft: "1rem",
+    flexWrap: "wrap",
+  },
+  information: {},
+  products: {},
+  delivery: {},
+  total: {
+    background: "white",
+    margin: "1rem 0 2rem 1rem",
+  },
 }));
 
 export default useStyles;
