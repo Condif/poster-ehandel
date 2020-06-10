93c483b1498b3641574d5e1764259e5388ac0ad4
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-06-10 10:02:46 +0200

    product stock empty and styling of card in cart

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -9,6 +9,7 @@ import {
   CardMedia,
   TextField,
   Typography,
+  Container,
 } from "@material-ui/core";
 import useStyles from "./ProductCardStyles";
 import { UserContext } from "../../Contexts/UserContext";
@@ -149,13 +150,16 @@ const ProductCard = (props) => {
         {props.case === "cart" || props.case === "checkout" ? (
           <div className={classes.flexedDiv}>
             <Button
+              className={classes.cartButtons}
               variant="contained"
               size="small"
               onClick={() => updateCounter(product, "add")}
+              disabled={product.inventory <= product.cartAmount}
             >
               <AddCircleOutlineIcon />
             </Button>
             <Button
+            className={classes.cartButtons}
               variant="contained"
               size="small"
               onClick={() => updateCounter(product, "remove")}
@@ -163,15 +167,24 @@ const ProductCard = (props) => {
               <RemoveCircleOutlineIcon />
             </Button>
             <Button
+              className={classes.cartButtons}
               variant="contained"
               size="small"
               onClick={() => deleteProduct(product)}
             >
               <DeleteIcon />
             </Button>
+            
           </div>
-        ) : null}
+          ) : null}
       </CardActions>
+      {props.case === "cart" || props.case === "checkout" ? (
+      <Container style={{display: "flex",  flexDirection: "column"}}>
+            {product.inventory <= product.cartAmount &&
+            <Typography>Product stock empty</Typography>
+            }
+      </Container>
+      ) : null}
     </Card>
   );
 };
